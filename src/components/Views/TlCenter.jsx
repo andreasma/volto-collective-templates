4cd993f4943ab17bfbbafbf7b2ff799176bc6c22
andreasma <maand@gmx.de>
2022-05-20 17:32:06 +0200

    Add sidebar container and ids and classes

--- src/components/Views/TlCenter.jsx
+++ src/components/Views/TlCenter.jsx
@@ -81,33 +81,37 @@ const TlCenterView = props => {
         </section>
       </article>
       </div>
-      <div className="four wide column">
-        <h2>{content.product_title}</h2>
-      <p>
-        {content.product_description && (
-            <span>{content.product_description}</span>
-          )}
-          <br />
-        <span>Currently there are </span>
-        <span>[...] project(s) available.</span></p>
-      <h3><Link to={'/add?type=tlproject'}>Submit Your Project</Link></h3>
-      <OwnProjects />
-      <h2>Most recenct {content.product_title}</h2>
-      <h2>Browse {content.product_title} by Category</h2>
-      <h3>Install Instructions</h3>
-      <h3>Reporting {content.product_title}-Bugs</h3>
-      <h3>About</h3>
-      <h3>Contact a Project Author</h3>
-      <p>You can send a message to the author of the project and 
-        give him feedback by following the link and fill in your 
-        information into the form:<br />
-        (...)
-        <br />
-        You could instead go to the page of the project and use 
-        the mail form linked from that page.
-      </p>
+      <div className='four wide column'>
+        <div id="sidebar">
+          <div className='splash pullout'>
+            <h2>{content.product_title}</h2>
+            <p>
+              {content.product_description && (
+              <span>{content.product_description}</span>
+              )}
+              <br />
+             <span>Currently there are </span>
+             <span>[...] project(s) available.</span></p>
+           <h3><Link to={'/add?type=tlproject'}>Submit Your Project</Link></h3>
+          </div>
+          <OwnProjects />
+          <h2 className='mostrecent'>Most recenct {content.product_title}</h2>
+          <h2 className='byCategory'>Browse {content.product_title} by Category</h2>
+          <h3>Install Instructions</h3>
+          <h3>Reporting {content.product_title}-Bugs</h3>
+          <h3>About</h3>
+          <h3>Contact a Project Author</h3>
+          <p>You can send a message to the author of the project and 
+             give him feedback by following the link and fill in your 
+             information into the form:<br />
+             (...)
+             <br />
+             You could instead go to the page of the project and use 
+             the mail form linked from that page.
+          </p>
+        </div>      
       </div>
-      </div>
+    </div>
     </Container>
   )
 };
